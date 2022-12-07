353f58ff12cbfb6ca1113281847cf2b66c684079
paytonshafer <paytonshafer@gmail.com>
2022-12-07 13:05:18 -0500

    first commit

--- src/main/java/com/example/calculator/HelloApplication.java
+++ src/main/java/com/example/calculator/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.calculator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +11,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        StackPane root=new StackPane();
+        Scene scene = new Scene(root.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
